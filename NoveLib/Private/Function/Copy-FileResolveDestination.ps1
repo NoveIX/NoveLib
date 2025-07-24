0ee283b28f7f938e51c8b1ffa077bd61a881994e
NoveIX <stefy.ferri2000@gmail.com>
2025-07-24 11:41:02 +0200

    Update Copy-FileResolveDestination.ps1

--- NoveLib/Private/Function/Copy-FileResolveDestination.ps1
+++ NoveLib/Private/Function/Copy-FileResolveDestination.ps1
@@ -20,7 +20,7 @@ function Copy-FileResolveDestination {
     [string]$relativePath = $File.FullName.Substring((Resolve-Path $Source).Path.Length)
     [string]$destPath = Join-Path -Path $Destination -ChildPath $relativePath
     [string]$destDir = Split-Path -Path $destPath -Parent
-    Test-Directory -Mode Ensure -Path $destDir | Out-Null
+    Test-Directory -Ensure -Path $destDir | Out-Null
 
     # Restore Hidden Attribute
     if ($isHidden) {
