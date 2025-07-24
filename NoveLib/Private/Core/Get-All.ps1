fbb78ecdb3b5e611f8fb3295085fbb684f0b8693
NoveIX <stefy.ferri2000@gmail.com>
2025-07-24 10:15:27 +0200

    Insert in Get-All Dir Hide and ReadOnly

--- NoveLib/Private/Core/Get-All.ps1
+++ NoveLib/Private/Core/Get-All.ps1
@@ -4,10 +4,10 @@ function Get-All {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true)]
-        [ValidateSet("Files", "Bytes")]
+        [ValidateSet('Files', 'Dir', 'Hide', 'ReadOnly', 'Bytes')]
         [string]$Mode,
 
-        [Parameter(Mandatory = $true, ParameterSetName = 'Files')]
+        [Parameter(Mandatory = $true, ParameterSetName = 'Files', 'Dir', 'Hide', 'ReadOnly')]
         [string]$Path,
 
         [Parameter(Mandatory = $true, ParameterSetName = 'Bytes')]
@@ -15,17 +15,35 @@ function Get-All {
     )
 
     switch ($Mode) {
-        "Files" {
-            if (-not $Path) {
-                throw "Path is required when Mode is 'Files'."
+        'Files' {
+            if ($Path) {
+                return Get-ChildItem -LiteralPath $Path -Recurse -File -Force
             }
-            return Get-ChildItem -LiteralPath $Path -Recurse -File -Force
+            throw "Path is required when Mode is 'Files'."
         }
-        "Bytes" {
-            if (-not $Array) {
-                throw "Array is required when Mode is 'Bytes'."
+        'Dir' {
+            if ($Path) {
+                return Get-ChildItem -LiteralPath $Path -Recurse -Directory -Force
             }
-            return ($Array | Measure-Object -Property Length -Sum).Sum
+            throw "Path is required when Mode is 'Files'."
+        }
+        'Hide' {
+            if ($Path) {
+                return Get-ChildItem -LiteralPath $Path -Recurse -Hidden -Force
+            }
+            throw "Path is required when Mode is 'Files'."
+        }
+        'ReadOnly' {
+            if ($Path) {
+                return Get-ChildItem -LiteralPath $Path -Recurse -ReadOnly -Force
+            }
+            throw "Path is required when Mode is 'Files'."
+        }
+        'Bytes' {
+            if ($Array) {
+                return ($Array | Measure-Object -Property Length -Sum).Sum
+            }
+            throw "Array is required when Mode is 'Bytes'."
         }
     }
 }
