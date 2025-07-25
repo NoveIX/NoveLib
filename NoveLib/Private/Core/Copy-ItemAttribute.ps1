cabca28b441c91e9e702e7642250071ebc22e077
NoveIX <stefy.ferri2000@gmail.com>
2025-07-25 11:00:06 +0200

    Create Copy-ItemAttribute.ps1

---
+++ NoveLib/Private/Core/Copy-ItemAttribute.ps1
@@ -0,0 +1,28 @@
+# File: NoveLib\Private\Core\Copy-ItemAttribute.ps1
+
+function Copy-ItemAttribute {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$Source,
+
+        [Parameter(Mandatory = $true)]
+        [string]$Destination
+    )
+
+    if (-not (Test-Path -LiteralPath $Source)) {
+        throw "Il percorso sorgente '$Source' non esiste."
+    }
+    if (-not (Test-Path -LiteralPath $Destination)) {
+        throw "Il percorso di destinazione '$Destination' non esiste."
+    }
+
+    [System.IO.FileSystemInfo]$sourceItem = Get-Item -LiteralPath $Source -Force
+    [System.IO.FileSystemInfo]$destinationItem = Get-Item -LiteralPath $Destination -Force
+
+    if ($sourceItem.PSIsContainer -ne $destinationItem.PSIsContainer) {
+        throw "Source e destination devono essere dello stesso tipo (entrambi file o entrambi cartelle)."
+    }
+
+    [System.IO.File]::SetAttributes($destinationItem.FullName, $sourceItem.Attributes)
+}
